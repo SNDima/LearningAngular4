c2eb76a79192a0533a26308794628ed05c9170ef
Dmitry Sirotkin <dsirotkin@servicechannel.com>
2017-11-01 19:14:27 +0300

    Redundant files were removed.

--- LearningAngular4/ClientApp/app/components/app/app.component.ts
+++ LearningAngular4/ClientApp/app/components/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    template: `Hello World!`
 })
 export class AppComponent {
 }
