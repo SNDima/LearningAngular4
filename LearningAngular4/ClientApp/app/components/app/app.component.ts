86c34b22cb1efb1dfc683a7d12bf3f7517daf552
Dmitry Sirotkin <dsirotkin@servicechannel.com>
2017-11-01 20:45:44 +0300

    Loops and if-else were learned.

--- LearningAngular4/ClientApp/app/components/app/app.component.ts
+++ LearningAngular4/ClientApp/app/components/app/app.component.ts
@@ -1,14 +1,18 @@
 ﻿import { Component } from '@angular/core';
 import { Book } from "../../entities/book";
+import { BOOKS } from "../../mocks/mock-books";
 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Мои книги';
-    book: Book = {
-        id: 1,
-        name: 'Старик и море'
+    title = 'Моя библиотека';
+    books = BOOKS;
+    selectedBook: Book;
+
+    onSelect(book: Book): void {
+        this.selectedBook = book;
     }
 }
