3cf9b04dcdea3eda945c30287565b972057f92ed
Dmitry Sirotkin <dsirotkin@servicechannel.com>
2017-11-01 19:55:21 +0300

    Bindings were learned.

--- LearningAngular4/ClientApp/app/components/app/app.component.ts
+++ LearningAngular4/ClientApp/app/components/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Book } from "../../entities/book";
 
 @Component({
     selector: 'app',
-    template: `Hello World!`
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
+    title = 'Мои книги';
+    book: Book = {
+        id: 1,
+        name: 'Старик и море'
+    }
 }
---
+++ LearningAngular4/ClientApp/app/entities/book.ts
@@ -0,0 +1,4 @@
+﻿export class Book {
+    id: number;
+    name: string;
+}
